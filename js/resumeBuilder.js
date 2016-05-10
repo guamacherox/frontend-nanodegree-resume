64dc3fa906c54cb68675751c9a7f605fb0b72579
ERK Soft <ERK Soft>
2016-05-10 09:57:03 -0400

    Aprendiendo Javascript

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// $("#main").append("Juan Luis Marval");
+
+/*
+var awesomeThoughts = "I am Juan and I am AWESOME!";
+var funThoghts = awesomeThoughts.replace("AWESOME","FUN");
+
+console.log(awesomeThoughts);
+console.log(funThoghts);
+
+$("#main").append(funThoghts);*/
+
+var formattedName = HTMLheaderName.replace("%data%","Juan Marval");
+var formattedRole = HTMLheaderRole.replace("%data%","Ingeniero en Computación");
+
+$("#header").append(formattedRole);
+$("#header").prepend(formattedName);
