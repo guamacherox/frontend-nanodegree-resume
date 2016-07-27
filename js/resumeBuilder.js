df20fd4849a1d958c777bfda0548763cb95524e0
Juan Marval <juanluismarval@gmail.com>
2016-07-27 16:43:49 -0400

    Cleaning up resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,5 +15,87 @@ $("#main").append(funThoghts);*/
 var formattedName = HTMLheaderName.replace("%data%","Juan Marval");
 var formattedRole = HTMLheaderRole.replace("%data%","Ingeniero en Computación");
 
+var skills = ["awesomeness","programming","teaching","Photoshop"];
+
+var bio = {
+  "name" : "Juan Marval",
+  "role" : "Ingeniero en Computación",
+  "contacts" : {
+    "mobile" : "04265836474",
+    "email" : "juanluismarval@gmail.com",
+    "github" : "guamacherox",
+    "twitter" : "@juanlmarval",
+    "location" : "El Guamache"
+  },
+  "welcomeMessage": "lorem ipsum dolor sit amet ipsum sit amet",
+  "skills": skills,
+  "bioPic": "images/fry.jpg"
+};
+
+var work = {
+  "works":
+  [
+    {
+      "company":"Movilnet",
+      "position":"Ejecutivo de Ventas",
+      "years": 5
+    },
+    {
+      "company": "ERKSoft",
+      "position": "Desarrollador Web",
+      "years": 2
+    }
+  ]
+};
+
+var education = {
+  "escuelas":
+  [
+    {
+      "school":"Universidad de Oriente",
+      "city":"Barcelona",
+      "Major":"Computer Science",
+      "Minor":"Web Development",
+      "graduation": 2008
+    },
+    {
+      "school":"Universidad de Oriente",
+      "city":"Barcelona",
+      "Major":"Computer Science",
+      "Minor":"Telecoms",
+      "graduation": 2010
+    }
+  ],
+  "onlineCourses":
+  [
+    {
+      "portal":"Udacity",
+      "name":"Javascript Basics"
+    },
+    {
+      "portal":"Codeschool",
+      "name":"Frontends Basics"
+    }
+  ]
+};
+
+var projects = {
+  "projects":[
+    {
+      "title": "Project Lorem",
+      "dates": "2014",
+      "description": "lorem ipsum dolor sit amet ipsum si amet lorem",
+      "images":[
+        "http://lorempixel.com/200/200/",
+        "http://lorempixel.com/200/200/"
+      ]
+    }
+  ]
+};
+
+
+
 $("#header").append(formattedRole);
 $("#header").prepend(formattedName);
+
+$("#main").append(skills.length);
