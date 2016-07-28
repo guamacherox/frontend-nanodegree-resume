00f2927fce041c344da6ffa157cd91e9fadc5702
Juan Marval <juanluismarval@gmail.com>
2016-07-28 19:48:56 -0400

    Internationalize Section Complete

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,10 +12,8 @@ console.log(funThoghts);
 
 $("#main").append(funThoghts);*/
 
-var formattedName = HTMLheaderName.replace("%data%","Juan Marval");
-var formattedRole = HTMLheaderRole.replace("%data%","Ingeniero en Computación");
 
-var skills = ["awesomeness","programming","teaching","Photoshop"];
+var skills = ["Awesomeness","Programming","Teaching","Photoshop"];
 
 var bio = {
   "name" : "Juan Marval",
@@ -29,54 +27,67 @@ var bio = {
   },
   "welcomeMessage": "lorem ipsum dolor sit amet ipsum sit amet",
   "skills": skills,
-  "bioPic": "images/fry.jpg"
+  "bioPic": "images/fry.jpg",
+  "display": "function(){}"
 };
 
 var work = {
   "works":
   [
     {
-      "company":"Movilnet",
-      "position":"Ejecutivo de Ventas",
-      "years": 5
+      "employer":"Movilnet",
+      "title": "Customer Support",
+      "location": "Anzoategui",
+      "dates": "2001",
+      "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
-      "company": "ERKSoft",
-      "position": "Desarrollador Web",
-      "years": 2
+      "employer": "ERKSoft",
+      "title": "Web Developer",
+      "location": "Anzoategui",
+      "dates": "2014",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
-  ]
+  ],
+  "display": ""
 };
 
 var education = {
-  "escuelas":
+  "schools":
   [
     {
-      "school":"Universidad de Oriente",
-      "city":"Barcelona",
-      "Major":"Computer Science",
-      "Minor":"Web Development",
-      "graduation": 2008
+      "name":"Universidad de Oriente",
+      "location":"Barcelona",
+      "degree": "",
+      "majors":["Computer Science"],
+      "dates": "19/02/2008",
+      "url": "http://loremuniversity.com"
     },
     {
-      "school":"Universidad de Oriente",
-      "city":"Barcelona",
-      "Major":"Computer Science",
-      "Minor":"Telecoms",
-      "graduation": 2010
+      "name":"Universidad de Oriente",
+      "location":"Barcelona",
+      "degree": "",
+      "majors":"Computer Science",
+      "dates": "19/02/2008",
+      "url": "http://loremuniversity.com"
     }
   ],
   "onlineCourses":
   [
     {
-      "portal":"Udacity",
-      "name":"Javascript Basics"
+      "title":"Javascript Basics",
+      "school":"Udacity",
+      "date":"12/01/2016",
+      "url": "http://loremsite.com"
     },
     {
-      "portal":"Codeschool",
-      "name":"Frontends Basics"
+      "title":"Javascript Basics",
+      "school":"Udacity",
+      "date":"12/01/2016",
+      "url": "http://loremsite.com"
     }
-  ]
+  ],
+  "display": ""
 };
 
 var projects = {
@@ -90,12 +101,48 @@ var projects = {
         "http://lorempixel.com/200/200/"
       ]
     }
-  ]
+  ],
+  "display":""
 };
 
-
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 
 $("#header").append(formattedRole);
 $("#header").prepend(formattedName);
 
-$("#main").append(skills.length);
+var displayWork = function(){
+
+  var formatedWorkEmployer = "";
+  var formatedWorkTitle = "";
+  var formatedJob = "";
+  var formatedDates = "";
+  var formatedLocation = "";
+  var formatedDesc = "";
+
+  for(var jobs in work.works){
+    $("#workExperience").append(HTMLworkStart);
+    formatedWorkEmployer = HTMLworkEmployer.replace("%data%", work.works[jobs].employer);
+    formatedWorkTitle = HTMLworkTitle.replace("%data%", work.works[jobs].title);
+    formatedJob = formatedWorkEmployer + formatedWorkTitle;
+    $(".work-entry:last").append(formatedJob);
+
+    formatedDate = HTMLworkDates.replace("%data%",work.works[jobs].dates);
+    $(".work-entry:last").append(formatedDate);
+
+    formatedLocation = HTMLworkLocation.replace("%data%", work.works[jobs].location);
+    $(".work-entry:last").append(formatedLocation);
+
+    formatedDesc = HTMLworkDescription.replace("%data%", work.works[jobs].description);
+    $(".work-entry:last").append(formatedDesc);
+  }
+};
+
+function inName(name){
+  name = name.trim().split(" ");
+  console.log(name);
+  name[1].toUpperCase();
+  name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+  return name[0] + " " + name[1];
+}
